091f4264aa85be2bd3eebc9f32775ff2f81bb259
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-01-30 11:16:52 -0500

    Initial commit

---
+++ src/LoopPractice.java
@@ -0,0 +1,16 @@
+public class LoopPractice {
+
+    public static void main(String[] args){
+                int rows = 9;
+
+                for(int i = 1; i <= rows; ++i)
+                {
+                    for(int j = 1; j <= i; ++j)
+                    {
+                        System.out.print(i + "\t");
+                    }
+                    System.out.println("");
+                }
+    }
+}
+
